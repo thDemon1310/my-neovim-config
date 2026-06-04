2da7c482e50afe166d4fd7e252557d4e8106fd45
Utplaksh Mishra <umishra8770@gmail.com>
2026-06-05 00:43:10 +0530

    added the cmd to update the package
    - Added the Packupdate to update the packege

--- lua/utplaksh/packageManager/command.lua
+++ lua/utplaksh/packageManager/command.lua
@@ -7,3 +7,15 @@ vim.api.nvim_create_user_command("PackDel", function(opts)
     vim.pack.del(opts.fargs)
 end, { nargs = "+", desc = "Delete plugins (:PackDel plugin1 plugin2)" })
 
+vim.api.nvim_create_user_command("PackUpdate", function(opts)
+	-- checks if any argument is passed
+    if opts.args:match("%S") then
+        -- update specific plugins
+        local plugins = vim.split(opts.args, "%s+", { trimempty = true })
+		-- update only specified plugins
+        vim.pack.update(plugins)
+    else
+        -- update all
+        vim.pack.update()
+    end
+end, { nargs = "*", desc = "Update all plugins or specific ones" })
