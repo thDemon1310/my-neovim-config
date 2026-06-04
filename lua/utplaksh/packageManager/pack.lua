29f5f6b94abd5c389839f1e50eba431495db9224
Utplaksh Mishra <umishra8770@gmail.com>
2026-06-05 00:44:41 +0530

    Added the lualine plugin for the better status line

--- lua/utplaksh/packageManager/pack.lua
+++ lua/utplaksh/packageManager/pack.lua
@@ -3,8 +3,13 @@ vim.pack.add({
     "https://github.com/rebelot/kanagawa.nvim",
     -- web devicons
     'https://github.com/nvim-tree/nvim-web-devicons',
+    -- lua line 
+    'https://github.com/nvim-lualine/lualine.nvim',
     -- nvim-tree
     'https://github.com/nvim-tree/nvim-tree.lua'
+
 })
+
 require("utplaksh.packageManager.plugins.colorTheme")
+require("utplaksh.packageManager.plugins.luaLine")
 require("utplaksh.packageManager.plugins.nvim-tree")
