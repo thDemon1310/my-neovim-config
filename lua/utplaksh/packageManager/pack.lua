5d03e11953310b21d483ba0a1bb6a38fd199f6f8
Utplaksh Mishra <umishra8770@gmail.com>
2026-06-05 00:44:18 +0530

    Added nvim-tree
    - added the nvim tree
    - configered it
    - disabled the netrw

--- lua/utplaksh/packageManager/pack.lua
+++ lua/utplaksh/packageManager/pack.lua
@@ -1,5 +1,10 @@
 vim.pack.add({
     -- Color scheme
     "https://github.com/rebelot/kanagawa.nvim",
+    -- web devicons
+    'https://github.com/nvim-tree/nvim-web-devicons',
+    -- nvim-tree
+    'https://github.com/nvim-tree/nvim-tree.lua'
 })
 require("utplaksh.packageManager.plugins.colorTheme")
+require("utplaksh.packageManager.plugins.nvim-tree")
