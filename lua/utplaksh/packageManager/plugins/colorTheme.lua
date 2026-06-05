fe73f17e968a76e0bc79afec72f25b9c296f096a
Utplaksh Mishra <umishra8770@gmail.com>
2026-06-05 16:23:38 +0530

    changed the select (visual) color

--- lua/utplaksh/packageManager/plugins/colorTheme.lua
+++ lua/utplaksh/packageManager/plugins/colorTheme.lua
@@ -15,7 +15,9 @@ require('kanagawa').setup({
         theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
     },
     overrides = function(colors) -- add/modify highlights
-        return {}
+        return {
+            Visual = {bg = "#2E3C64", fg = "none" }
+        }
     end,
     theme = "wave",              -- Load "wave" theme
     background = {               -- map the value of 'background' option to a theme
