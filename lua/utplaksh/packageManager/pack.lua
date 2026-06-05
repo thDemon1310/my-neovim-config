26a63dac68dcd3993792b682bcf680eec7299f34
Utplaksh Mishra <umishra8770@gmail.com>
2026-06-05 16:26:15 +0530

    Added telescope to nvim config
    - configered the telescope

--- lua/utplaksh/packageManager/pack.lua
+++ lua/utplaksh/packageManager/pack.lua
@@ -8,8 +8,17 @@ vim.pack.add({
     -- nvim-tree
     'https://github.com/nvim-tree/nvim-tree.lua',
 
+    -- === TELESCOPE & DEPENDENCIES ===
+    -- 1. Plenary (Required dependency for Telescope)
+    'https://github.com/nvim-lua/plenary.nvim',
+    -- 2. Telescope core
+    'https://github.com/nvim-telescope/telescope.nvim',
+    -- 3. FZF Native (Optional but highly recommended for speed)
+    'https://github.com/nvim-telescope/telescope-fzf-native.nvim',
+
 })
 
 require("utplaksh.packageManager.plugins.colorTheme")
 require("utplaksh.packageManager.plugins.luaLine")
 require("utplaksh.packageManager.plugins.nvim-tree")
+require("utplaksh.packageManager.plugins.telescope")
